5bf88ce8699e8d0639bfa47139bb08fc38a203a0
wxyice <920082206@qq.com>
2021-10-19 19:36:04 +0800

    fix bug

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from math import acos,cos,asin,sin,atan
-from math import e,pi
-import numpy as np
 import random
+from math import acos, atan, e, pi, sin
+from matplotlib import pyplot as plt
+
+
 
 
 def cal_F(r,R,n,r_hub,phi):
@@ -46,32 +47,6 @@ def cal_f(n,r,v1,R,r_hub,a,a1):
     #print('f={}'.format(f))
     return f
 
-
-def cal_Cp2max(a,r,R,n,r_hub,v1):
-    
-    err=100
-    deltaA=0.0001
-
-    count=0
-    a_new=a+deltaA
-    while err>0.00001:
-        
-        df=cal_Cp(a_new,r,R,n,r_hub,v1)-cal_Cp(a,r,R,n,r_hub,v1)
-
-        if df<0:
-            deltaA = -deltaA
-        a=a_new
-        a_new=a+deltaA
-
-        err=abs(df)
-        print('err={}'.format(err))
-        print(a_new)
-        count+=1
-        if count>300:
-            print('break')
-            break
-    return a_new,cal_Cp(a_new,r,R,n,r_hub,v1)
-
 def cal_a1(a,r,R,n,r_hub,v1):
     dx=1e-6
     err=1000
@@ -80,14 +55,12 @@ def cal_a1(a,r,R,n,r_hub,v1):
         dy=(cal_f(n,r,v1,R,r_hub,a, a1+dx)-cal_f(n,r,v1,R,r_hub,a, a1))/dx
         a1=a1-cal_f(n,r,v1,R,r_hub,a, a1)/(dy+1e-8)
         err=abs(cal_f(n,r,v1,R,r_hub,a, a1))
-    #print("a1={0:8.4f}".format(a1))
     return a1
 
 def cal_CPmax(r,R,n,r_hub,v1):
     err=100
     da=0.0001
     a=random.random()
-    #count=0
     learning_rate=0.001
 
     while True:
@@ -95,37 +68,97 @@ def cal_CPmax(r,R,n,r_hub,v1):
 
         a_new = a + dCp * learning_rate
         err=abs(a_new-a)  
-        print('err={}'.format(err))   
-        if err<1e-4:
+        #print('err={}'.format(err))   
+        if err<1e-6:
             break
         else:
             a=a_new
-            print("a={}".format(a_new))
-        # count+=1
-        # if count>300:
-        #     print('break')
-        #     break
-    return a_new,cal_Cp(a_new,r,R,n,r_hub,v1)
+            #print("a={}".format(a_new))
 
+    return a_new,cal_a1(a_new,r,R,n,r_hub,v1),cal_Cp(a_new,r,R,n,r_hub,v1)
+
+
+# def cal_Cp2max(a,r,R,n,r_hub,v1):
+    
+#     err=100
+#     deltaA=0.0001
+
+#     count=0
+#     a_new=a+deltaA
+#     while err>0.00001:
+        
+#         df=cal_Cp(a_new,r,R,n,r_hub,v1)-cal_Cp(a,r,R,n,r_hub,v1)
+
+#         if df<0:
+#             deltaA = -deltaA
+#         a=a_new
+#         a_new=a+deltaA
+
+#         err=abs(df)
+#         print('err={}'.format(err))
+#         print(a_new)
+#         count+=1
+#         if count>300:
+#             print('break')
+#             break
+#     return a_new,cal_Cp(a_new,r,R,n,r_hub,v1)
+
+# def cal_a1(a,r,R,n,r_hub,v1):
+#     dx=1e-6
+#     err=1000
+#     a1=0
+#     while err>1e-6:
+#         dy=(cal_f(n,r,v1,R,r_hub,a, a1+dx)-cal_f(n,r,v1,R,r_hub,a, a1))/dx
+#         a1=a1-cal_f(n,r,v1,R,r_hub,a, a1)/(dy+1e-8)
+#         err=abs(cal_f(n,r,v1,R,r_hub,a, a1))
+#     #print("a1={0:8.4f}".format(a1))
+#     return a1
+
+# def cal_CPmax(r,R,n,r_hub,v1):
+#     err=100
+#     da=0.0001
+#     a=random.random()
+#     #count=0
+#     learning_rate=0.001
+
+#     while True:
+#         dCp=(cal_Cp(a+da,r,R,n,r_hub,v1)-cal_Cp(a,r,R,n,r_hub,v1))/da
+
+#         a_new = a + dCp * learning_rate
+#         err=abs(a_new-a)  
+#         print('err={}'.format(err))   
+#         if err<1e-4:
+#             break
+#         else:
+#             a=a_new
+#             print("a={}".format(a_new))
+#         # count+=1
+#         # if count>300:
+#         #     print('break')
+#         #     break
+#     return a_new,cal_Cp(a_new,r,R,n,r_hub,v1)
 if __name__ == '__main__':
 
-    # a1=cal_a1(a=0.34,r=20,R=70,n=72,r_hub=2,v1=13)
-    a,cp=cal_CPmax(r=20,R=70,n=72,r_hub=2,v1=13)
-    print(a,cp)
-    #print(a1)
-    # a11=[]
-    # f11=[]
-    # for a1 in range(0,30):
-    #     a1=a1/1000
-    #     f=cal_f(n=10,r=30,v1=13,R=40,r_hub=15,a=0.30,a1=a1)
-    #     f11.append(f)
-    #     a11.append(a1)
-    
-    # import numpy as np
-    # from matplotlib import pyplot as plt
+    rlist=[]
+    alist=[]
+    Cplist=[]
+    a1list=[]
 
-    # plt.plot(a11,f11)
-    # plt.show()
+    for i in range(4,72,4):
+        a,a1,cp=cal_CPmax(r=i,R=70,n=72,r_hub=2,v1=13)
+        rlist.append(i)
+        alist.append(a)
+        a1list.append(a1)
+        
+        print("r={3},a={0},a1={1},cp={2}".format(a,a1,cp,i))
+
+    plt.plot(rlist,a1list)
+    plt.show()
+    
+
+    
+
+
 
 
 
