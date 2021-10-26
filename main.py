d1d399eacf9aa083f913de6f8ec47335606892bf
wxyice <920082206@qq.com>
2021-10-26 15:03:15 +0800

    2021.10.26

--- main.py
+++ main.py
@@ -7,11 +7,20 @@ import numpy as np
 
 def cal_F(r,R,n,r_hub,phi):
     """
-    :param: r 半径位置 m
-    :param: R 风轮半径 m
-    :param: n 转速     rps
-    :param: r_hub 轮毂半径 m
-    :param: phi 入流角 rad
+    This is a groups style docs.
+    
+    Parameters:
+    param1 - r 半径位置 m 
+    param2 - R 风轮半径 m
+    param3 - n 转速     rps
+    param4 - r_hub 轮毂半径 m
+    param5 - phi 入流角 rad
+
+    Returns:
+    F
+    
+    Raises:
+    KeyError - raises an exception
     """
     Fte=e**(-n*(R-r)/(2*R*sin(phi)))
     Ft=2*acos(Fte)/pi
@@ -39,15 +48,29 @@ def cal_phi(a,a1,lamda):
     phi=atan((1-a)/(lamda*(1+a1)))
     return phi
 
-def cal_f(n,r,v1,R,r_hub,a,a1):
-    lamda=(n*2*pi)*r/v1
-    phi=cal_phi(a,a1,lamda)
-    F=cal_F(r,R,n,r_hub,phi)
-    f=a1*(1+a1)*(lamda**2)-(a*(1-a*F))
-    #print('f={}'.format(f))
-    return f
+def cal_l(a,F,phi,n,r,Cl):
+    k1=8*pi*a*F*(1-a*F)*sin(phi)**2
+    k2=(1-a)**2*cos(phi)
+    
+    k3=r/(n*Cl)
+
+    return k1/k2*k3
+
+def cal_sigma(a,F,phi):
+    k1=a*F*(1-a*F)*sin(phi)**2
+    k2=(1-a)**2*cos(phi)
+    return k1/k2
 
 def cal_a1(a,r,R,n,r_hub,v1):
+
+    def cal_f(n,r,v1,R,r_hub,a,a1):
+        lamda=(n*2*pi)*r/v1
+        phi=cal_phi(a,a1,lamda)
+        F=cal_F(r,R,n,r_hub,phi)
+        f=a1*(1+a1)*(lamda**2)-(a*(1-a*F))
+        #print('f={}'.format(f))
+        return f
+
     dx=1e-6
     err=1000
     a1=0
@@ -75,26 +98,10 @@ def cal_CPmax(r,R,n,r_hub,v1):
         else:
             a=a_new
             #print("a={},iter={}".format(a_new,iternum))
-        # if iternum>10000:
-        #     a=random.random()
-        #     iternum=0
     return a_new,cal_a1(a_new,r,R,n,r_hub,v1),cal_Cp(a_new,r,R,n,r_hub,v1)
-# l=2
-# sigma=0.05
-# 4/5 pi
 
-def cal_l(a,F,phi,n,r,Cl):
-    k1=8*pi*a*F*(1-a*F)*sin(phi)**2
-    k2=(1-a)**2*cos(phi)
-    
-    k3=r/(n*Cl)
 
-    return k1/k2*k3
 
-def cal_sigma(a,F,phi):
-    k1=a*F*(1-a*F)*sin(phi)**2
-    k2=(1-a)**2*cos(phi)
-    return k1/k2
 
 
 if __name__ == '__main__':
@@ -105,12 +112,12 @@ if __name__ == '__main__':
     Cl=1.0423
     omega=n*2*pi
 
-    skip=100
+    step=30
     delta=0.01
 
 
-    # a a1 cp 
-    rlist=iter(np.linspace(r_hub+delta,R-delta,skip))
+    #------------------------calculate---------------------------
+    rlist=iter(np.linspace(r_hub+delta,R-delta,step))
     alist=[]
     Cplist=[]
     a1list=[]
@@ -121,24 +128,16 @@ if __name__ == '__main__':
         except :
             break
         a,a1,cp=cal_CPmax(r=r,R=R,n=n,r_hub=r_hub,v1=v1)
-        #rlist.append(i)
         alist.append(a)
         a1list.append(a1)
         Cplist.append(cp)
 
-        print("r={3},a={0},a1={1},cp={2}".format(a,a1,cp,r))
+        print("r={3:6.4f} a={0:6.4f} a1={1:6.4f} cp={2:6.4f}".format(a,a1,cp,r))
 
-
-    # F phi l 
-    rlist=np.linspace(r_hub+delta,R-delta,skip)
-    rlist=rlist.tolist()
+    rlist=np.linspace(r_hub+delta,R-delta,step).tolist()
     aa1iter=iter(zip(rlist,alist,a1list))
 
-    philist=[]
-    Flist=[]
-    llist=[]
-    sigmalist=[]
-    lamdalist=[]
+    philist,Flist,llist,sigmalist,lamdalist=[],[],[],[],[]
 
     while True:
         try:
@@ -156,17 +155,18 @@ if __name__ == '__main__':
         Flist.append(F)
         llist.append(l)
         sigmalist.append(sigma)
+
+        print("r={3:6.4f} lamda={0:6.4f} phi={1:6.4f} F={2:6.4f} l={4:6.4f} sigma={5:6.4f}".format(lamda,phi,F,r,l,sigma))
     
     
     av1list=np.array(alist)*v1
-    omegarlist=np.linspace(r_hub+delta,R-delta,skip)*omega*np.array(a1list)
+    omegarlist=np.linspace(r_hub+delta,R-delta,step)*omega*np.array(a1list)
     W=(v1*np.sqrt((1-np.array(alist))**2+(1+np.array(a1list))**2*np.array(lamdalist)**2)).tolist()
     av1list=av1list.tolist()
     omegarlist=omegarlist.tolist()
 
-    #--------------------draw--------------------------------
-
-    rlist=np.linspace(r_hub+delta,R-delta,skip)
+    #--------------------------save data as txt-------------------------
+    rlist=np.linspace(r_hub+delta,R-delta,step)
 
     value=[alist,a1list,Flist,philist,Cplist,llist,sigmalist,av1list,omegarlist,W]
     with open('result.txt','w') as f:
@@ -178,7 +178,7 @@ if __name__ == '__main__':
 
     list1=[rlist,alist,a1list,Flist,philist,Cplist,llist,sigmalist,av1list,omegarlist,W]
     
-    
+    #--------------------------save data as xls-------------------------------
     with open('data.xls','w',encoding='gbk') as output:
 
         for i in range(len(list1)):
@@ -188,7 +188,7 @@ if __name__ == '__main__':
             output.write('\n')       
     output.close()
 
-
+    #-----------------------------draw---------------------------------------
     for i in range(len(value)):
         plt.subplot(5,2,i+1)
         plt.plot(rlist,value[i])
